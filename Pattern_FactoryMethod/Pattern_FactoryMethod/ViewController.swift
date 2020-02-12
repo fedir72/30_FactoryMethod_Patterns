e3246ca168b0eda85846be6dddce87f346dad7ed
fedii <fedir72@ua.fm>
2020-02-12 21:38:31 +0200

    start project

---
+++ Pattern_FactoryMethod/Pattern_FactoryMethod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pattern_FactoryMethod
+//
+//  Created by MAC on 2/12/20.
+//  Copyright © 2020 MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
